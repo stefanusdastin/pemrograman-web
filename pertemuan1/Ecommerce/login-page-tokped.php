d12aba1535d018ca82e8ab50081fe1aa9779bbcc
stefanusdastin <114202495+stefanusdastin@users.noreply.github.com>
2022-10-17 21:19:33 +0700

    finishing tampilan, rename file

--- pertemuan1/Ecommerce/login-page-tokped.php
+++ pertemuan1/Ecommerce/login-page-tokped.php
@@ -25,7 +25,7 @@ if (isset($_COOKIE['email'])) {
     <!-- Nav Bar -->
     <nav class="navbar navbar-expand-lg navbar-light shadow-lg p-2">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">
+            <a class="navbar-brand" href="http://localhost/pemrograman-web/pertemuan1/Ecommerce/landing-tokped.php">
                 <img src="img/logo.svg" alt="" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,6 +39,7 @@ if (isset($_COOKIE['email'])) {
     <!-- Form -->
     <!-- pastikan action menuju ke auth.php dan method POST/post -->
     <section id="form">
+
         <div class="row text-center mb-3">
             <div class="col mt-5">
                 <h2>Login</h2>
@@ -60,6 +61,7 @@ if (isset($_COOKIE['email'])) {
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
                             <input name="password" type="password" class="form-control" type="password">
+                            <p class="form-text">Already have an account?<a href="#">Sign In</a><br><a href="#">Forgot your password?</a></p>
                         </div>
                     </div>
                 </div>
@@ -73,10 +75,48 @@ if (isset($_COOKIE['email'])) {
     </section>
     <!-- Akhir Form -->
 
+    <!-- Footer -->
+    <div class="container">
+        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+            <div class="col mb-3">
+                <a class="navbar-brand" href="http://localhost/pemrograman-web/pertemuan1/Ecommerce/index-include-tokped.php">
+                    <img src="img/logo.svg" alt="" />
+                </a>
+                <p class="text-muted">© 2022</p>
+            </div>
+
+            <div class="col mb-3"></div>
+
+            <div class="col mb-3"></div>
+
+            <div class="col mb-3">
+                <h5>Menu</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">For You</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Explore</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Category</a></li>
+                </ul>
+            </div>
+
+            <div class="col mb-3">
+                <h5>Others</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Helps</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contacts</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+    <!-- Akhir Footer -->
+
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
--- pertemuan1/Ecommerce/logout-process-tokped.php
+++ pertemuan1/Ecommerce/logout-process-tokped.php
@@ -5,7 +5,7 @@ if (isset($_SESSION['email'])) {
     session_destroy();
     echo '<script>
         alert("Anda berhasil logout");
-        window.location="index-include-tokped.php";
+        window.location="landing-tokped.php";
     </script>';
 } else {
     header("location:index-include-tokped.php");
